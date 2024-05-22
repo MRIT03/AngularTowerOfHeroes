2ad3ae6b939bb5a4affb4837f296e2106c9289bb
Riad Terek <Riadot03@gmail.com>
2024-05-23 02:17:33 +0300

    Added a list of heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { NgFor} from '@angular/common';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  hero: Hero ={
+      name: 'Windstorm',
+      id:1,
+  } ;
+  heroes = HEROES;
+}
